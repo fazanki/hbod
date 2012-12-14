fa58b3f903dc4b183c4dde9f1076eaab52038ba3
Franco Zanki <zankian.vision@gmail.com>
2012-12-14 16:11:59 +0000

    new module

--- sites/all/themes/framework/templates/html.tpl.php
+++ sites/all/themes/framework/templates/html.tpl.php
@@ -37,5 +37,20 @@
             HRBOT.Sitewide.Controller.init();
         }
     </script>
+
+    <script type="text/javascript">
+
+    //   var _gaq = _gaq || [];
+    //   _gaq.push(['_setAccount', 'UA-37059057-1']);
+    //   _gaq.push(['_setDomainName', 'hbod.hr']);
+    //   _gaq.push(['_trackPageview']);
+
+    //   (function() {
+    //     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    //     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    //     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    //   })();
+
+     </script>
     
 </html>
